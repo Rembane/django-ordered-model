a214283a924e149fc2bc808c572d234493da90e4
Rembane <andeke@gmail.com>
2013-04-12 22:01:04 +0200

    Refactors the _move-method and adds a compact_order-function.
      * The _move-method of OrderedModel now uses update of the manager to
        not trigger the save-method of the inheriting models.
      * The compact_order-function removes gaps in the orders.

--- ordered_model/models.py
+++ ordered_model/models.py
@@ -2,6 +2,15 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.db import models
 
+def compact_order(manager):
+    u"""Removes gaps between orders. [2,3,5,7] becomes: [0,1,2,3]."""
+
+    last = -1
+    for (pk, order) in manager.values_list('pk', 'order').order_by('order'):
+        if order - last > 1:
+            manager.filter(pk=pk).update(order=last + 1)
+        last += 1
+
 class OrderedModel(models.Model):
     """
     An abstract model that allows objects to be ordered relative to each other.
@@ -9,7 +18,7 @@ class OrderedModel(models.Model):
     """
     
     order = models.PositiveIntegerField(editable=False)
-    
+
     class Meta:
         abstract = True
         ordering = ('order',)
@@ -24,20 +33,24 @@ class OrderedModel(models.Model):
         super(OrderedModel, self).save(*args, **kwargs)
     
     def _move(self, up):
-        qs = self.__class__._default_manager
+        manager = self.__class__._default_manager
         if up:
-            qs = qs.order_by('-order').filter(order__lt=self.order)
+            qs = manager.order_by('-order').filter(order__lt=self.order)
         else:
-            qs = qs.filter(order__gt=self.order)
+            qs = manager.filter(order__gt=self.order)
         try:
             replacement = qs[0]
         except IndexError:
             # already first/last
             return
-        self.order, replacement.order = replacement.order, self.order
-        self.save()
-        replacement.save()
-    
+
+        # Save the re-ordered objects bypassing the save methods to not 
+        # trigger save-signals, custom save methods  and other weirdnesses.
+        manager.filter(pk=self.pk).update(order=replacement.order)
+        manager.filter(pk=replacement.pk).update(order=self.order)
+
+        compact_order(manager)
+
     def move_down(self):
         """
         Move this object down one position.
